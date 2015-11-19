4269ec418e14e3e3811cb44635d58cc655b1a95f
changeworld <changesworlds@gmail.com>
2015-11-20 00:34:03 +0900

    Modify Dockerfile
    - Install packages for Ruby, npm, Node.js
    - Update-alternative nodojs to node
    - Update npm
    - Install rbenv and ruby-build
    - Install multiple versions of ruby
    - Install Bundler for each version of ruby
    - Install the agent installer
    - Create Agents
    close #1

--- spec/server/apt-get_spec.rb
+++ spec/server/apt-get_spec.rb
@@ -1,7 +1,53 @@
 require 'spec_helper'
 
 describe 'apt-get' do
-  describe package('nginx') do
+  describe package('build-essential') do
     it { should be_installed }
+  end
+
+  describe package('curl') do
+    it { should be_installed }
+  end
+
+  describe package('git') do
+    it { should be_installed }
+  end
+
+  describe package('libreadline-dev') do
+    it { should be_installed }
+  end
+
+  describe package('libssl-dev') do
+    it { should be_installed }
+  end
+
+  describe package('libyaml-dev') do
+    it { should be_installed }
+  end
+
+  describe package('libxml2-dev') do
+    it { should be_installed }
+  end
+
+  describe package('nodejs') do
+    it { should be_installed }
+  end
+
+  describe package('npm') do
+    it { should be_installed }
+  end
+
+  describe package('zlib1g-dev') do
+    it { should be_installed }
+  end
+
+  # Update-alternatives nodojs to node
+  describe command('which node') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  # Update npm to the latest version?
+  describe command('npm -v') do
+    its(:stdout) { should_not match /1.3.10/ }
   end
 end
